7a265e327889ac0b740e1f303a5ae84321af6852
Roger <logroger@gmail.com>
2021-05-22 19:00:15 -0300

    first commit

---
+++ src/main/java/io/github/rogerlog/documentacaoswagger/DocumentacaoSwaggerApplication.java
@@ -0,0 +1,13 @@
+package io.github.rogerlog.documentacaoswagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentacaoSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentacaoSwaggerApplication.class, args);
+	}
+
+}
