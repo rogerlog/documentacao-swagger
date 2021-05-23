85d1cb7c62c0943a4b35652acd49637456ce96cc
Roger <logroger@gmail.com>
2021-05-23 19:36:12 -0300

    project config

---
+++ src/main/java/io/github/rogerlog/documentacaoswagger/config/TestConfig.java
@@ -0,0 +1,30 @@
+package io.github.rogerlog.documentacaoswagger.config;
+
+import com.nelioalves.cursomc.services.DBService;
+import com.nelioalves.cursomc.services.EmailService;
+import com.nelioalves.cursomc.services.MockEmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+	@Autowired
+	private DBService dbService;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new MockEmailService();
+	}
+}
